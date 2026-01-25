04cebe8a727077c035adfedc0773dd68197b5f27
maximilianschroder <maxi.schroeder99@web.de>
2026-01-25 09:44:12 +0100

    initial setup

---
+++ src/main/java/com/iu/ipwa/herotozero/HerotozeroApplication.java
@@ -0,0 +1,13 @@
+package com.iu.ipwa.herotozero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerotozeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerotozeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iu/ipwa/herotozero/HerotozeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iu.ipwa.herotozero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerotozeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
